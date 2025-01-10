8fd1ed341a5c6ce784e0402f047343bbfb5f8163
RicardoMunoz <ricardomunoz4250@gmail.com>
2025-01-09 21:08:38 -0800

    items array created and rendered unstyled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+let itemsForSale = [
+  { id: crypto.randomUUID(), icon: "📱", name: "Iphone", price: "$99" },
+  { id: crypto.randomUUID(), icon: "💻", name: "Laptop", price: "$1999" },
+  { id: crypto.randomUUID(), icon: "📷", name: "Camera", price: "$699" },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Merchandise />
     </div>
   );
 }
 
-export default App;
+function Merchandise() {
+  const items = itemsForSale;
+  const itemsQuantity = itemsForSale.length;
+  return (
+    <main className="merch">
+      <h1>Ricardo's Shop</h1>
+
+      {itemsQuantity > 0 ? (
+        <ul>
+          {items.map((item) => (
+            <Item itemObj={item} key={item.id} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're fresh out of inventory. Please visit back</p>
+      )}
+    </main>
+  );
+}
+
+function Item({ itemObj }) {
+  return (
+    <div className="item">
+      <li>
+        <p>{itemObj.icon}</p>
+        <p>{itemObj.name}</p>
+        <p>{itemObj.price}</p>
+      </li>
+    </div>
+  );
+}
+
+function Button({ children }) {
+  <button>{children}</button>;
+}
+
+// function ShoppingPage() {}
+
+// function Cart(itemCount) {}
